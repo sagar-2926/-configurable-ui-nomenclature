b2338cc85c4cff0397ec1227802a7d10120c1bf8
sagar-wandile <wandilesagar2926@gmail.com>
2023-10-02 11:07:39 +0530

    config json adeed and sum changes

--- src/index.js
+++ src/index.js
@@ -1,17 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-
+import Home from './view/Home/Home';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import 'fontawesome'
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+ <>
+ <i class="fa-light fa-cloud-showers-heavy fa-bounce"></i>
+ <Home />
+ </>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
---
+++ src/view/Home/Home.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import './Home.css';
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+function Home() {
+  return (
+    <>
+    <h1>Home</h1>
+      <div className="container p-5">
+     
+     </div>
+    
+    </>
+     
+  
+  )
+}
+
+export default Home
